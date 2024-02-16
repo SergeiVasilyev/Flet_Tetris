5b3ad5387a5cae61eeac8ce73fab47c0be81b3e9
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2024-02-16 17:32:43 +0200

    settings

--- settings.py
+++ settings.py
@@ -1,22 +1,22 @@
 import flet as ft
 
-
+# Board
 board_width = 10
 board_height = 20
 board = []
 
+# Tetromino blocks
 box_width = 20
 box_height = 20
 box_margin = 3
 box_border_radius = 2
-
-page_padding = 0
-horizontal_offset = 4
-tetromino_blocks_positions = []
-element = None
-
-main_container = None
-
 mute_color = ft.colors.GREY_500
 bright_color = ft.colors.BLACK
+
+# Game init settings
+page_padding = 0
+horizontal_offset = 4
+
+
+
 
