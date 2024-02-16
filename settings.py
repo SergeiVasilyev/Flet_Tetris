7a70a75f054c85019ad35a9fcf500332f5a938a6
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2024-02-16 10:07:08 +0200

    settings

---
+++ settings.py
@@ -0,0 +1,16 @@
+import flet as ft
+
+
+board_width = 10
+board_height = 20
+board = []
+
+page_padding = 0
+horizontal_offset = 4
+tetromino_blocks_positions = []
+element = None
+
+
+mute_color = ft.colors.GREY_500
+bright_color = ft.colors.BLACK
+
