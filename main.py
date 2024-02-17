a9b51520fe39aaa0468586390220122b99a875cf
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2024-02-17 22:50:34 +0200

    main updated

--- main.py
+++ main.py
@@ -2,96 +2,45 @@ import asyncio
 from functools import reduce
 import flet as ft
 from tetrominoes import TETROMINOES
-from initialization import background, init, generate_element
 from settings import *
+from utils import Tetris
+from initialization import Initialization
 
 
+init = Initialization()
 
-row_position = -1
-col_position = 3
-
-board = init()
-main_container = background()
-tetramino = TETROMINOES[generate_element()]
-tetromino_blocks_positions = []
+main_container = init.background()
+board = init.init()
+tetramino = TETROMINOES[init.generate_element()]
 
 
+tetris = Tetris(main_container, board, tetramino, row_position=ROW_POSITION, col_position=COL_POSITION)
 
-def draw(drawing):
-    global board, tetromino_blocks_positions, row_position, col_position, tetramino
-    tetromino_blocks_positions = []
-    for x, y in tetramino:
-        board[y+row_position][x+col_position] = 1 if drawing else 0
-        tetromino_blocks_positions.append([(y+row_position)*10+(x+col_position), y+row_position, x+col_position])
-    return board
 
-def vertical_collision():
-    global tetromino_blocks_positions
-    for i in tetromino_blocks_positions:
-        if i[1] >= 19:
-            return True
-        elif board[i[1]+1][i[2]] == -1:
-            return True
-    return False
-
-def mark_as_dropped():
-    for j in tetromino_blocks_positions:
-        board[j[1]][j[2]] = -1 # mark as dropped
-
-def horizontal_collision(x):
-    for i in tetromino_blocks_positions:
-        if x + i[2] < 0 or x + i[2] > 9:
-            return True
-    return False
-
-def element_length(tetramino):
-    max_len = 0
-    res = reduce(lambda x, y: [x[i] or y[i] for i in range(len(x))], tetramino)
-    max_len = sum(res)
-    return max_len
-
-def is_full():
-    if -1 in board[1]:
-        return True
-    return False
-
-def path_correction(new_element):
-    global tetramino, col_position, row_position
-    for el in new_element:
-        if el[0] + col_position > 9:
-            col_position = 9 - el[0]
-        elif el[0] + col_position < 0:
-            col_position = 0 - el[0]
-        elif el[1] + row_position > 19:
-            row_position = 19 - el[1]
-        elif el[1] + row_position < 0:
-            row_position = 0 - el[1]
-    return new_element
 
 async def main(page: ft.Page):
-    global board, tetromino_blocks_positions, row_position, col_position, tetramino, main_container
 
     def draw_element(drawing):
-        global board, tetromino_blocks_positions, main_container
-        draw(drawing)
-        print(tetromino_blocks_positions)
-        for el in tetromino_blocks_positions:
+        tetris.draw(drawing)
+        # print(tetris.tetromino_blocks_positions)
+        for el in tetris.tetromino_blocks_positions:
             main_container.content.controls[el[0]].border = ft.border.all(2, bright_color if drawing else mute_color)
             main_container.content.controls[el[0]].content.controls[0].bgcolor = bright_color if drawing else mute_color
 
     async def refresh_bord():
         for i in range(20):
             for j in range(10):
-                main_container.content.controls[i*10+j].border = ft.border.all(2, bright_color if board[i][j] == -1 else mute_color)
-                main_container.content.controls[i*10+j].content.controls[0].bgcolor = bright_color if board[i][j] == -1 else mute_color
+                main_container.content.controls[i*10+j].border = ft.border.all(2, bright_color if tetris.board[i][j] == -1 else mute_color)
+                main_container.content.controls[i*10+j].content.controls[0].bgcolor = bright_color if tetris.board[i][j] == -1 else mute_color
         await page.update_async()
 
-    def remove_line(row):
-        while row != 0:
-            board[row] = board[row - 1][:]
-            row -= 1
-
+    
     async def is_line_complete():
+        def remove_line(row):
+            while row != 0:
+                board[row] = board[row - 1][:]
+                row -= 1
+
         for i in range(board_height):
             if -1 in board[i]:
                 if abs(sum(board[i])) == board_width:
@@ -100,74 +49,78 @@ async def main(page: ft.Page):
         return True
 
     async def animate(e):
-        global row_position, col_position, tetramino
-        if row_position != -1:
+        if tetris.row_position != -1:
             draw_element(False)
-        row_position += 1
+        tetris.row_position += 1
         draw_element(True)
-        if is_full():
-            init(True)
-        if vertical_collision():
-            mark_as_dropped()
+        if tetris.is_full():
+            tetris.board = init.init(True)
+            tetris.main_container = init.background()
+            tetris.tetramino = TETROMINOES[init.generate_element()]
+            tetris.reset_position()
+        if tetris.vertical_collision():
+            tetris.mark_as_dropped()
             await is_line_complete()
-            row_position = -1
-            col_position = 3
-            tetramino = TETROMINOES[generate_element()]
+            tetris.reset_position()
+            tetris.refresh_tetromino()
         await page.update_async()
 
     async def game(e):
-        global row_position, col_position, tetramino
+        print(button.on_click)
         while True:
             await animate(None)
             await asyncio.sleep(1)
             
             
     async def drop(e):
-        global row_position
-        while row_position != -1:
+        while tetris.row_position != -1:
             await animate(None)
 
 
     async def restart(e):
-        global main_container
-        init(True)
+        tetris.board = init.init(True)
+        tetris.refresh_tetromino()
+        tetris.reset_position()
         await page.update_async()
     
-    
     async def rotate(e):
-        global tetramino
-        if row_position >= 0:
+        if tetris.row_position >= 0:
             draw_element(False)
-            rotated_element = [(-y, x) for x, y in tetramino]
-            tetramino = path_correction(rotated_element)
+            rotated_element = [(-y, x) for x, y in tetris.tetramino]
+            tetris.tetramino = tetris.path_correction(rotated_element)
             draw_element(True)
             await page.update_async()
 
     async def left(e):
-        global col_position
-        if row_position >= 0:
+        if tetris.row_position >= 0:
             draw_element(False)
-            if not horizontal_collision(-1):
-                col_position -= 1
+            if not tetris.horizontal_collision(-1):
+                tetris.col_position -= 1
             draw_element(True)
             await page.update_async()
 
     async def right(e):
-        global col_position
-        if row_position >= 0:
+        if tetris.row_position >= 0:
             draw_element(False)
-            if not horizontal_collision(1):
-                col_position += 1
+            if not tetris.horizontal_collision(1):
+                tetris.col_position += 1
             draw_element(True)
             await page.update_async()
     
 
-    button = ft.ElevatedButton("START!", on_click=game)
-    drop_btn = ft.ElevatedButton("Drop!", on_click=drop)
-    restart_btn = ft.ElevatedButton("Restart!", on_click=restart)
+    # async def test(e):
+    #     global data_test
+    #     print(test_btn.data)
+    #     test_btn.data = not test_btn.data
+        
+
+    button = ft.ElevatedButton("START", on_click=game, data=True)
+    drop_btn = ft.ElevatedButton("Drop", on_click=drop)
+    restart_btn = ft.ElevatedButton("Restart", on_click=restart)
     rotate_btn = ft.ElevatedButton("Rotate", on_click=rotate)
-    left_btn = ft.ElevatedButton("Left", on_click=left)
+    left_btn = ft.ElevatedButton("Left", on_click=left) # on_long_press
     right_btn = ft.ElevatedButton("Right", on_click=right)
+    # test_btn = ft.ElevatedButton("TEST", on_click=test, data=True)
 
     buttons = ft.Row([
         button,
@@ -179,7 +132,8 @@ async def main(page: ft.Page):
         left_btn,
         right_btn,
         drop_btn,
-        rotate_btn
+        rotate_btn,
+        # test_btn
     ],
     alignment=ft.MainAxisAlignment.CENTER)
 
