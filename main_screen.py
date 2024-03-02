9e3cdbf89f0fbc959bd8cd226e1532515991c4ba
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2024-03-02 22:10:26 +0200

    next_tetromino_viewer() added

--- main_screen.py
+++ main_screen.py
@@ -5,17 +5,19 @@ from settings import *
 
 
 class MainScreen():
+
+    def __init__(self):
+        self.main_cont_padding = BOX_MARGIN*2
+        self.main_cont_width = (BOX_WIDTH + BOX_MARGIN) * 10 + self.main_cont_padding
+        self.main_cont_height = (BOX_HEIGHT + BOX_MARGIN) * 20 + self.main_cont_padding
+
     def background(self):
-        main_cont_padding = BOX_MARGIN*2
-        main_cont_width = (BOX_WIDTH + BOX_MARGIN) * 10 + main_cont_padding
-        main_cont_height = (BOX_HEIGHT + BOX_MARGIN) * 20 + main_cont_padding
+        sidebar = ft.Container(width=150, height=self.main_cont_height, padding=self.main_cont_padding, bgcolor="#afb582")
 
-        sidebar = ft.Container(width=150, height=main_cont_height, padding=main_cont_padding, bgcolor="#afb582")
-
-        main_container = ft.GridView(expand=True, max_extent=BOX_WIDTH, child_aspect_ratio=1, spacing=main_cont_padding, run_spacing=main_cont_padding, padding=0)
+        main_container = ft.GridView(expand=True, max_extent=BOX_WIDTH, child_aspect_ratio=1, spacing=self.main_cont_padding, run_spacing=self.main_cont_padding, padding=0)
 
         main_screen_row = ft.Row([
-            ft.Container(main_container, width=main_cont_width, padding=main_cont_padding, bgcolor="#afb582"), 
+            ft.Container(main_container, width=self.main_cont_width, padding=self.main_cont_padding, bgcolor="#afb582"), 
             sidebar],
             alignment=ft.MainAxisAlignment.CENTER,
             spacing=0,
@@ -36,3 +38,32 @@ class MainScreen():
             )
 
         return main_screen_row
+    
+
+    def next_tetromino_viewer(self):
+        width = (BOX_WIDTH + BOX_MARGIN) * 4 + self.main_cont_padding
+        # height = (BOX_HEIGHT + BOX_MARGIN) * 2 + self.main_cont_padding
+
+        next_container_grid = ft.GridView(expand=True, max_extent=BOX_WIDTH, child_aspect_ratio=1, spacing=self.main_cont_padding, run_spacing=self.main_cont_padding, padding=0)
+        next_container = ft.Container(next_container_grid, width=width, padding=self.main_cont_padding, bgcolor="#afb582")
+
+        stack = ft.Stack()
+        stack.controls = [ft.Container(width=INNER_BOX_WIDTH, height=INNER_BOX_HEIGHT, bgcolor=MUTE_COLOR, border_radius=BOX_BORDER_RADIUS)]
+
+        for _ in range(8):
+            next_container_grid.controls.append(
+                ft.Container(
+                    alignment=ft.alignment.center,
+                    border=ft.border.all(2, MUTE_COLOR),
+                    border_radius=ft.border_radius.all(BOX_BORDER_RADIUS),
+                    content=copy.deepcopy(stack)
+                )
+            )
+        return next_container
+    
+
+if __name__ == '__main__':
+    m = MainScreen()
+    n = m.next_tetromino_viewer()
+    n.content.controls[0].border = ft.border.all(2, BRIGHT_COLOR)
+    
