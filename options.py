467fa27d1d67962aed8408c0ef53f17958e5d18c
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2024-03-14 20:17:30 +0200

    class Options updated

--- options.py
+++ options.py
@@ -7,20 +7,23 @@ from settings import *
 class Options:
     def __init__(self) -> None:
         self.title = ft.Text("OPTIONS", size=30, color="black")
-        self.clockwise = ft.Switch()
+        self.clockwise_label = ft.Text(f"{OPTIONS_LABELS[0]}", size=16)
+        self.clockwise = ft.Switch(value=True)
         self.reset_highscrore_label = ft.Text(f"{OPTIONS_LABELS[1]} 0", size=16)
         self.reset_highscrore = ft.ElevatedButton("RESET")
+        self.save_game_label = ft.Text(f"{OPTIONS_LABELS[2]}", size=16)
         self.save_game = ft.ElevatedButton("SAVE")
+        self.load_game_label = ft.Text(f"{OPTIONS_LABELS[3]}", size=16)
         self.load_game = ft.ElevatedButton("LOAD")
 
 
     def options_fn(self, container_width, container_height) -> ft.Column:
         title = ft.Container(content=self.title, padding=10, alignment=ft.alignment.center)
 
-        clockwise = ft.Container(content=ft.Row([ft.Text("CLOCKWISE", size=16), self.clockwise], alignment=ft.MainAxisAlignment.SPACE_BETWEEN))
+        clockwise = ft.Container(content=ft.Row([self.clockwise_label, self.clockwise], alignment=ft.MainAxisAlignment.SPACE_BETWEEN))
         reset_highscrore = ft.Container(content=ft.Row([self.reset_highscrore_label, self.reset_highscrore], alignment=ft.MainAxisAlignment.SPACE_BETWEEN))
-        save_game = ft.Container(content=ft.Row([ft.Text("SAVE GAME", size=16), self.save_game], alignment=ft.MainAxisAlignment.SPACE_BETWEEN))
-        load_game = ft.Container(content=ft.Row([ft.Text("LOAD GAME", size=16), self.load_game], alignment=ft.MainAxisAlignment.SPACE_BETWEEN))
+        save_game = ft.Container(content=ft.Row([self.save_game_label, self.save_game], alignment=ft.MainAxisAlignment.SPACE_BETWEEN))
+        load_game = ft.Container(content=ft.Row([self.load_game_label, self.load_game], alignment=ft.MainAxisAlignment.SPACE_BETWEEN))
 
         credits_label = ft.Container(content=ft.Text("Autor: Sergey Vasilev, 2024 \nGitHub: https://github.com/SergeiVasilyev/", size=10, color="black"), )
 
